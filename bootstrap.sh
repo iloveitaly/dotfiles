f62177affc16debc5fba59566e90162d4d95a131
Michael Bianco <iloveitaly@gmail.com>
2014-11-13 10:03:00 -0500

    Adding custom mysql config
    Also restarting apache

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,13 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" \
   	--exclude ".osx" \
   	--exclude ".brew" \
+  	--exclude "my.cnf" \
   	--exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "ssh_config" -av . ~
 fi
+
 cp -f ./ssh_config ~/.ssh/config
+cp -f ./my.cnf /etc/my.cnf
 source "$HOME/.bash_profile"
+
+sudo apachectl start
+mysql.server restart || sudo killall mysqld
