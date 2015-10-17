d2e04ca3272d73f8df176ae4d5feab61ef546d27
Michael Bianco <iloveitaly@gmail.com>
2015-10-17 14:20:03 -0400

    Finally! I found a decent postgres GUI

--- cask.sh
+++ cask.sh
@@ -72,6 +72,8 @@ brew cask install rightfont
 brew cask install messenger
 brew cask install gitter
 brew cask install scrivener
+brew cask install postico
+brew cask install focus
 
 # TODO test if CC is already installed
 open "/opt/homebrew-cask/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app"
