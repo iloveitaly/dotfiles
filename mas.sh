d9284d6b3c81c83048d9f2b97bf601f00cb4667f
Michael Bianco <iloveitaly@gmail.com>
2017-01-04 06:37:06 -0700

    Create mas.sh

---
+++ mas.sh
@@ -0,0 +1,11 @@
+mas install 409789998 # twitter
+
+# business account `mas signin email`
+mas install 585829637 # todoist
+mas install 407963104 # pixelmator
+mas install 896450579 # textual
+mas install 439697913 # Icon Slate
+mas install 430255202 # Mactracker
+mas install 431511738 # Timing
+mas install 483014855 # Browser Fairy
+mas install 1124077199 # Paws for Trello
