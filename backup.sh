385c69d7e8e84bf3e583cfbbf880220ea61170b7
Michael Bianco <iloveitaly@gmail.com>
2017-01-05 11:15:11 -0700

    Adding script to backup postgres and mysql

--- backup.sh
+++ backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 mackup backup
+
 apm star --installed
 
+# these files need to be manually copied to the new machine
+pg_dumpall > ~/Desktop/postgres_backup
+mysqldump -u root -p --all-databases > ~/Desktop/mysql_backup.sql
