6f6ba832a20e067d909f2136a05e3a5b562e3835
Michael Bianco <iloveitaly@gmail.com>
2018-12-21 06:47:33 -0700

    Some manual backup instructions for unsupported apps

--- backup.sh
+++ backup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# manual
+#   - Alfred Configuration Backup. Sync to cloud storage.
+#   - Google Chrome. Sync to user profile.
+#   - 1Password. Support was never merged, sensitive application to muck with permissions. https://github.com/lra/mackup/pull/1080
+#   - Dash. Save preferences and snippets in-app.
+#   - SSH and GPG
+
 mackup backup
 
 apm star --installed
 
-# these files need to be manually copied to the new machine
+# manually copy files to the new machine
 pg_dumpall > ~/Desktop/postgres_backup
 mysqldump -u root -p --all-databases > ~/Desktop/mysql_backup.sql
