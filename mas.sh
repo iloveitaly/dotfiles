4e11b7ec3b8f6df7c0c880d2633eee0c52a3a791
Michael Bianco <iloveitaly@gmail.com>
2020-06-17 08:11:54 -0600

    Updating mas installs

--- mas.sh
+++ mas.sh
@@ -1,24 +1,20 @@
 # business account `mas signin email`
-mas install 585829637 # todoist
-mas install 407963104 # pixelmator
 mas install 896450579 # Textual
 mas install 439697913 # Icon Slate
 mas install 430255202 # Mactracker
-mas install 431511738 # Timing
-mas install 483014855 # Browser Fairy
 mas install 1278508951 # Trello
-mas install 1172952443 # Refly Editor
-mas install 1017470484 # Next Meeting
 mas install 1063631769 # Medis - GUI for Redis
 mas install 1173932628 # Drop - Color Picker
+
+# personal account
+mas install 585829637 # Todoist
+mas install 986304488 # Kiwi for Gmail
+mas install 1017470484 # Next Meeting
+mas install 1456226992 # CurrentKey Stats
+mas install 588726889 # ReadKit
 
 # safari extensions
 mas install 1180442868 # SmileAllDay
 mas install 1472777122 # Honey
 mas install 1474298973 # Buffer
 mas install 588726889 # ReadKit
-
-# Other
-# Hemmingway OS X
-# TODO find better distraction tracking tools
-# TODO find better habit tracking, maybe something that ties in with alexa?
