2acd21ae3f1706bbb27930f07490467c322861ad
Michael Bianco <iloveitaly@gmail.com>
2021-04-16 09:55:02 -0600

    Fixing up bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,12 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" \
   	--exclude "osx.sh" \
   	--exclude "brew.sh" \
+    --exclude "cask.sh" --exclude "mas.sh" \
+    --exclude "vscode-extensions.txt" --exclude "vscode-settings.json" \
     --exclude "apache.conf" \
-    --exclude "my.cnf" \
-    --exclude "cask.sh" \
     --exclude "backup.sh" \
-  	--exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "ssh_config" -av . ~
+  	--exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "ssh_config" \
+    -av . ~
 fi
 
 mackup restore
@@ -21,14 +22,8 @@ cp ./vscode-settings.json "$HOME/Library/Application Support/Code/User/settings.
 
 cp -f ./ssh_config ~/.ssh/config
 
-sudo cp -f my.cnf /etc/my.cnf
-
-# TODO install extensiopns via requirements.txt
-# TODO document vscode customizations
-code --install-extension
-
 # block distracting websites
-# some additional configuration is required here
+# some additional configuration is required here:
 # http://mikebian.co/how-to-block-distracting-websites-on-your-laptop/
-sed '/^$/d' ./distracting_websites.txt | sed $'s/\(.*\)/127.0.0.1  \\1\\\n127.0.0.1  www.\\1/' > ~/distracting_sites.txt
+sed '/^$/d' ./distracting_websites.txt | sed -E $'s/\(.*\)/127.0.0.1  \\1\\\n127.0.0.1  www.\\1/' > ~/distracting_sites.txt
 sudo -E `asdf which node` `asdf which hostile` load ~/distracting_sites.txt
