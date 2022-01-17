0cfc0051fd269985f15bbcd2828e7d1009b9361e
Michael Bianco <iloveitaly@gmail.com>
2022-01-17 06:45:41 -0700

    Removing some older macos apps I don't use anymore

--- mas.sh
+++ mas.sh
@@ -5,7 +5,6 @@ mas install 430255202 # Mactracker
 mas install 1278508951 # Trello
 mas install 1063631769 # Medis - GUI for Redis
 mas install 1173932628 # Drop - Color Picker
-mas install 896450579 # Textual IRC Client (6.0.12)
 
 # personal account
 mas install 585829637 # Todoist
@@ -13,8 +12,8 @@ mas install 986304488 # Kiwi for Gmail
 mas install 1017470484 # Next Meeting
 mas install 1456226992 # CurrentKey Stats
 mas install 588726889 # ReadKit
+mas install 963034692 # Streaks
 
 # safari extensions
 mas install 1180442868 # SmileAllDay
-mas install 1472777122 # Honey
 mas install 1474298973 # Buffer
