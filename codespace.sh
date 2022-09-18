c5a6f186ac5e248754fada0d3d26aa2caf878719
Michael Bianco <iloveitaly@gmail.com>
2022-09-18 06:30:59 -0600

    More filtering on codespace sync, executable

--- codespace.sh
+++ codespace.sh
@@ -3,13 +3,13 @@
 echo "Syncing dotfiles..."
 rsync --exclude ".git/" \
       --exclude "osx.sh" \
-      --exclude "brew.sh" \
-      --exclude "duti" \
+      --exclude={'brew.sh','Brewfile'} \
+      --exclude={'duti','distracting_websites.txt'} \
       --exclude "cask.sh" --exclude "mas.sh" \
       --exclude ipython_config.py \
-      --exclude "vscode-extensions.txt" --exclude "vscode-settings.json" \
+      --exclude={'vscode-extensions.txt','vscode-keybindings.json','vscode-settings.json'} \
       --exclude "backup.sh" \
-      --exclude "bootstrap.sh" --exclude "README.md" --exclude "ssh_config" \
+      --exclude={'bootstrap.sh','codespace.sh','README.md','ssh_config'} \
       -av . ~
 
 brew bundle
