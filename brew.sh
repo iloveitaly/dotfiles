d18b61b52508461540d17b27fd98afbdc90f2f32
Michael Bianco <iloveitaly@gmail.com>
2023-01-03 10:55:15 -0700

    improved nanorc syntax highlighting install

--- brew.sh
+++ brew.sh
@@ -27,7 +27,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 (cd ~/.config/ && curl -LO https://raw.githubusercontent.com/sindresorhus/iterm2-snazzy/master/Snazzy.itermcolors) && open ~/.config/Snazzy.itermcolors
 
 # run after nanorc is copied, this modifies nanorc
-curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | bash -s -- --lite
 
 # iTerm copy mode conflicted with a previous shortcut that I have seared into my memory
 # however, I can remap this via macos config. TODO would be great to move this over to `osx.sh`
