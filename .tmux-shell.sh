68e26ed6616e4329b00843e37a8f0773f142575d
Michael Bianco <iloveitaly@gmail.com>
2023-05-21 06:57:23 -0600

    adding tmux shell cmd
    should probably use one of the tmux plugins, but chatgpt helped with me with this one and it's easy to understand

---
+++ .tmux-shell.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env zsh
+
+# initial session name
+session_name=${PWD:t}
+
+# session counter
+counter=0
+
+while [[ $counter -lt 10 ]]; do
+  if [[ $counter -gt 0 ]]; then
+    session="${session_name} $counter"
+  else
+    session="${session_name}"
+  fi
+
+  # if the session doesn't exist, create it
+  if ! tmux has-session -t "$session" 2>/dev/null; then
+    tmux new -ADs "$session"
+    break
+  fi
+
+  # if the session exists but isn't attached, attach it
+  if [ -z "$(tmux list-sessions -F "#{session_name} #{session_attached}" | grep "^$session 1")" ]; then
+    tmux attach -t "$session"
+    break
+  fi
+
+  counter=$((counter + 1))
+done
+
+if [[ $counter -ge 10 ]]; then
+  echo "Could not find an unattached session after 10 attempts."
+fi
