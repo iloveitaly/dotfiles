1787280f5faf6a1810adea408c2e635674ddab6f
Michael Bianco <iloveitaly@gmail.com>
2023-06-27 15:00:28 -0600

    hyper focus config update

--- .config/focus/initial_wake.sh
+++ .config/focus/initial_wake.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env zsh
 
+# assume the script is executed without common environment variables
+# additionally, since it's not executed in login mode, ~/.zshrc is not loaded
+export USER=mike
+export HOME=/Users/$USER
+source ~/.asdf/asdf.sh
+
 # close work related apps on sunday
 work_apps=()
 if [ $(date +%u) -eq 7 ]; then
+  echo "It's sunday, closing additional apps"
   work_apps=(TablePlus "Visual Studio Code" Postico Kaleidoscope)
 fi
 
-apps=($work_apps Slack Discord Podcasts "Amazon Music" zoom.us ReadKit Texts Messages Gmail "System Settings" Music)
+apps=(
+  $work_apps
+  Slack Mattermost GitHub
+  Discord
+  Podcasts "Amazon Music" Spotify
+  Dictionary Notes
+  zoom.us ReadKit Readwise Texts Messages Gmail "System Settings" Music
+)
+
 for app in $apps; do
   echo "Quitting $app..."
 
@@ -28,14 +43,10 @@ return text returned of dialogResult
 EOT
 )
 
-# when executed as part of a sleepwatcher, the environment is not set up
-export HOME=/Users/mike
-source $HOME/.zshrc
-
 # cleanup browser tabs
 echo "Cleaning browser tabs..."
 cd ~/Projects/clean-workspace
-poetry run clean-workspace "$dialogResult"
+sudo poetry run clean-workspace "$dialogResult"
 
 # organize todoist
 echo "Organizing todoist..."
