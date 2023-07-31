a001826b8501090c970f4640f6f7cd739715d050
Michael Bianco <iloveitaly@gmail.com>
2023-07-31 11:40:48 -0600

    add EDITOR magic param to open in cli editor instead of vs code

--- .open-file-path.sh
+++ .open-file-path.sh
@@ -2,4 +2,11 @@
 # Description: hook for tmux-fastcopy to open files, that contain like + column references, in vscode
 
 source ~/.functions
-open_in_vscode $@
+
+# if first argument is EDITOR, remove it from $@
+if [[ $1 == "EDITOR" ]]; then
+  shift
+  open_in_nano $@
+else
+  open_in_vscode $@
+fi
