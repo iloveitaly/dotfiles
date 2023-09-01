8197867558ef2551e120e2dd1180828d4235a9f4
Michael Bianco <iloveitaly@gmail.com>
2023-09-01 07:00:23 -0600

    simplify vscode tmux integration

--- .tmux-shell.sh
+++ .tmux-shell.sh
@@ -1,37 +1,24 @@
 #!/usr/bin/env zsh
+# Description: tmux launcher for vs code which will restore a terminal session based on the directory it was opened in
 
-copy_vscode_env_vars() {
-  local session=$1
-
-  # Copy VSCODE_ environment variables to the tmux session
-  for var in ${(k)parameters}; do
-    if [[ $var == VSCODE_* ]]; then
-      tmux set-environment -t "$session" "$var" "${(P)var}"
-    fi
-  done
-}
-
-session_name=${PWD:t}
+session_uid=${PWD:t}
 counter=0
 
+# sleep for a random amount of time between 0 and 2 seconds
+# sleep $(((RANDOM % 2000)/1000.0))
+
 while [[ $counter -lt 10 ]]; do
-  if [[ $counter -gt 0 ]]; then
-    session="${session_name} $counter"
-  else
-    session="${session_name}"
-  fi
+  session="${session_uid}-$counter"
 
   # if the session doesn't exist, create it
   if ! tmux has-session -t "$session" 2>/dev/null; then
     tmux new -ADs "$session"
-    copy_vscode_env_vars "$session"
     break
   fi
 
   # if the session exists but isn't attached, attach it
   if [ -z "$(tmux list-sessions -F "#{session_name} #{session_attached}" | grep "^$session 1")" ]; then
-    tmux attach -t "$session"
-    copy_vscode_env_vars "$session"
+    tmux attach -dt "$session"
     break
   fi
 
