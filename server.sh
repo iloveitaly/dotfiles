3285d6d58eeb9b324c2bfd5adcdda75cac089f96
Michael Bianco <iloveitaly@gmail.com>
2023-09-24 07:28:18 -0600

    update server install

--- server.sh
+++ server.sh
@@ -4,21 +4,13 @@ rsync --exclude ".git/" \
     --exclude={"osx.sh","Brewfile","Brewfile.lock.json","brew.sh","server.sh","TODO","duti","backup.sh"} \
     --exclude={"distracting_websites.txt","DefaultKeyBinding.dict","aw-categories.json"} \
     --exclude={'bootstrap.sh','codespace.sh','README.md','TODO'} \
+    --exclude={'.tmux.conf','.tool-versions','.asdfrc'} \
+    --exclude={'.ssh','mackup.cfg','.config/karabiner/','.config/gmailctl/','.config/focus/','.config/extrakto/'} \
     -av . ~
 
 # for homebrew to actually work
 # sudo apt-get install build-essential procps curl file git gobjc++ glibc-source
 
-echo "Syncing dotfiles..."
-rsync --exclude ".git/" \
-      --exclude "osx.sh" \
-      --exclude={'brew.sh','Brewfile'} \
-      --exclude={'duti','distracting_websites.txt'} \
-      --exclude "cask.sh" --exclude "mas.sh" \
-      --exclude ipython_config.py \
-      --exclude={'vscode-extensions.txt','vscode-keybindings.json','vscode-settings.json'} \
-      --exclude "backup.sh" \
-      --exclude={'bootstrap.sh','codespace.sh','README.md','ssh_config'} \
-      -av . ~
+eval $(/home/linuxbrew/bin/brew shellenv)
 
 brew bundle
