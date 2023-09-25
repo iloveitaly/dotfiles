fcec909cc8b546d43b712f6d6bbeb03b0bba2dec
Michael Bianco <iloveitaly@gmail.com>
2023-09-25 08:02:44 -0600

    check for homebrew
    helpful for ansible testsing

--- install/server.sh
+++ install/server.sh
@@ -6,12 +6,21 @@ rsync --exclude-from="install/standard-exclude.txt" \
       --exclude-from="install/server-exclude.txt" \
       -av . ~
 
-# TODO should support more linuxbrew locations
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+local homebrew_path="/home/linuxbrew/.linuxbrew/bin/brew"
+
+if [ ! -f "$homebrew_path" ]; then
+  echo "No homebrew installation detected"
+  exit 1
+fi
+
+eval $("$homebrew_path" shellenv)
 
 brew bundle
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
+
+# cleaner output since this will be running inside ansible, or something similar
+export ZINIT_COLORIZE=false
 
 # run zinit to install all plugins
 zsh -lc "source ~/.zshrc && zinit update --parallel"
