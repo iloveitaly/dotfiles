18ebe2d6ecc33cb0478b4f1bb2688d85b4cc3b3d
Michael Bianco <iloveitaly@gmail.com>
2023-10-15 05:45:38 -0600

    kill tmux sessions, don't keep them around

--- .tmux-shell.sh
+++ .tmux-shell.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env zsh
-# Description: tmux launcher for vs code which will restore a terminal session based on the directory it was opened in
+# Description: tmux launcher for vs code which will name the sessions
 
 session_uid=${PWD:t}
 counter=0
-
-# sleep for a random amount of time between 0 and 2 seconds
-# sleep $(((RANDOM % 2000)/1000.0))
 
 while [[ $counter -lt 10 ]]; do
   session="${session_uid}-$counter"
@@ -16,15 +13,5 @@ while [[ $counter -lt 10 ]]; do
     break
   fi
 
-  # if the session exists but isn't attached, attach it
-  if [ -z "$(tmux list-sessions -F "#{session_name} #{session_attached}" | grep "^$session 1")" ]; then
-    tmux attach -dt "$session"
-    break
-  fi
-
   counter=$((counter + 1))
 done
-
-if [[ $counter -ge 10 ]]; then
-  echo "Could not find an unattached session after 10 attempts."
-fi
