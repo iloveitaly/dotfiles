31c31040b30028705388d1b9490dd5cd2c30719f
Michael Bianco <iloveitaly@gmail.com>
2024-08-11 07:36:34 -0600

    don't clobber ipy namespace with extension

--- .ipython/profile_default/startup/rich_inspect.py
+++ .ipython/profile_default/startup/rich_inspect.py
@@ -1,9 +1,11 @@
 # https://rich.readthedocs.io/en/stable/introduction.html#rich-inspect
-def inspect(o):
+def _ipython_rich_inspect(o):
     import rich
 
     rich.inspect(o, methods=True, docs=True, private=True)
 
 
 # i(a) or i a
-get_ipython().user_ns["i"] = inspect
+get_ipython().user_ns["i"] = _ipython_rich_inspect
+
+del _ipython_rich_inspect
