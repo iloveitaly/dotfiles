9a362e8a791a9f0b359d48fcb8f162ff07405217
Michael Bianco <mike@mikebian.co>
2025-07-02 09:32:28 -0600

    fix: source functions after hostile block to ensure env setup
    Generated-by: aiautocommit

--- .config/focus/wake.sh
+++ .config/focus/wake.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env zsh
 
 # you'll need to run this as root to modify the /etc/hosts file
-# test with `sudo env -i wake.sh`
+# test with `sudo env -i wake.sh` which replicates a `sudo` hyper-focus execution
 
 export HOME=/Users/mike
 export PATH="$HOME/.local/bin:/sbin:/usr/sbin:/opt/homebrew/bin:$PATH"
 
-source ~/.config/focus/functions.sh
-
 $(mise which hostile) load ~/.config/distracting_sites.txt
 
+source ~/.config/focus/functions.sh
 flush-dns
