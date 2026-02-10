af3c9546167125c0e4638dddbe02ba62445d2c0a
Michael Bianco <mike@mikebian.co>
2026-02-10 06:39:34 -0700

    build: delegate launch agent setup to just configure-launchagents
    Generated-by: aiautocommit

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,13 +27,7 @@ infat
 mkdir -p ~/Library/KeyBindings/
 cp ./DefaultKeyBinding.dict ~/Library/KeyBindings/
 
-# TODO link to blog post
-cp ./tmux.plist ~/Library/LaunchAgents/mikebianco.tmux.plist
-tmux kill-server
-launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/mikebianco.tmux.plist
-
-cp ./pty.plist ~/Library/LaunchAgents/mikebianco.pty.plist
-launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/mikebianco.pty.plist
+just configure-launchagents
 
 # some tools can setup bashrc and other files that we don't want
 # for instance, homebrew will setup a bashrc file. In order to provide a completely clean bash environment, we remove it.
