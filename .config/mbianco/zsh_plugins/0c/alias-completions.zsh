2c4314f2cadea44d0a86d76ef8452bea4064d9ed
Michael Bianco <mike@mikebian.co>
2026-09-12 13:22:52 -0600

    fix(zsh): guard alias completions with command existence checks
    Generated-by: aiautocommit

--- .config/mbianco/zsh_plugins/0c/alias-completions.zsh
+++ .config/mbianco/zsh_plugins/0c/alias-completions.zsh
@@ -2,9 +2,17 @@
 # underlying command's completer. Loaded from 0c/ (wait'0c'); zicdreplay
 # runs after this directory is sourced.
 
-zicompdef fdd=fd
-zicompdef fdu=fd
-zicompdef rgg=rg
-zicompdef rgu=rg
-zicompdef rgc=rg
-zicompdef dk=dokku
+if (( $+commands[fd] )); then
+  zicompdef fdd=fd
+  zicompdef fdu=fd
+fi
+
+if (( $+commands[rg] )); then
+  zicompdef rgg=rg
+  zicompdef rgu=rg
+  zicompdef rgc=rg
+fi
+
+if (( $+commands[dokku] )); then
+  zicompdef dk=dokku
+fi
